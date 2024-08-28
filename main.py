38416aefc48becd6b0a45bd903ac29251b311879
XingLoong <xing.loong@gmail.com>
2024-08-28 16:22:51 +1000

    5: add cells

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import _tkinter
 from window import *
 from line import *
 from point import *
+from cell import *
 
 def main():
     win = Window(800, 600)
-    point1 = Point(1, 1)
-    point2 = Point(200, 200)
-    line = Line(point1, point2)
-    win.draw_line(line, "red")
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(50, 50, 100, 100)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(255, 255, 300, 300)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(255, 325, 300, 400)
+
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 125, 100, 175)
+
     win.wait_for_close()
 
 main()
